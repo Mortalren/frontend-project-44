0124592dd104eff92e199ebe2b78c5be397d13dc
Mortalren <sobakafuga@gmail.com>
2025-03-07 13:26:27 +0700

    Delete src/index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { greetingMessage } from './cli.js';
-
-import { evenNumber } from './even.js';
-
-import { generateRandomExpression } from './calc.js';
-
-import { getResultGcd } from './src/gcd.js';
-
-import { progressionGame } from './src/progression.js';
