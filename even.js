9b4a209862e00d704f5ae992790d8ec977dd8793
Mortalren <sobakafuga@gmail.com>
2026-02-11 06:53:14 +0700

    Add files via upload

---
+++ even.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+
+export const evenNumber = (name) => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".')
+  
+  const questionsCount = 3
+  
+  for (let i = 0; i < questionsCount; i += 1) {
+    const min = 1;
+    const max = 100;
+    const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    console.log(`Question: ${randomNum}`);
+    const userAns = readlineSync.question('Your answer: ')
+    
+    const isEven = randomNum % 2 === 0;
+    const correctAnswer = isEven ? 'yes' : 'no'
+    
+    if (userAns !== 'yes' && userAns !== 'no') {
+      console.log(`'${userAns}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`)
+      return;
+    }
+    
+    if ((isEven && userAns === 'yes') || (!isEven && userAns === 'no')) {
+      console.log('Correct!')
+    } else {
+      console.log(`'${userAns}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+      console.log(`Let's try again, ${name}!`)
+      return;
+    }
+  }
+  
+  console.log(`Congratulations, ${name}!`)
+}
