46c60c2edd74e5fb4d9af52004008ff7876db5e0
Mortalren <sobakafuga@gmail.com>
2026-02-11 08:03:52 +0700

    Delete utils.js directory

--- utils.js/prostoe.js
+++
@@ -1,7 +0,0 @@
-const IsSimple = (num)=> {
-    for(let i = 2; i < num; i++){
-        if (num % i === 0) return false;
-    } return num !== 1;
-}
-
-export default IsSimple
--- utils.js/randomn.js
+++
@@ -1,9 +0,0 @@
-const getRandomNumber = (
-  min = 1,
-  max = 100,
-) => {
-  const randomNumber = min + Math.random() * (max + 1 - min);
-  return Math.floor(randomNumber);
-};
-
-export default getRandomNumber;
